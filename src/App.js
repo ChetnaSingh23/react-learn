e5f61f042dd26f404730c9f7d41700c0757fc963
Chetna Singh <Chetnasingh03052312@gmail.com>
2020-07-05 21:44:34 +0530

    Initial commit :tada:

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to new react app
+          Welcome to home page
         </p>
         
       </header>
